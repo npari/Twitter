5a213232855779a9147449ecd8dc50a9c9ab7b20
Nithya Pari <Nithya_Pari@intuit.com>
2017-04-17 03:16:34 -0700

    Tweets table view

--- Twitter/TweetsViewController.swift
+++ Twitter/TweetsViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class TweetsViewController: UIViewController {
+class TweetsViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tweetsTableView: UITableView!
+    
     var tweets : [Tweet] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tweetsTableView.dataSource = self
+        tweetsTableView.rowHeight = UITableViewAutomaticDimension
+        tweetsTableView.estimatedRowHeight = 120
         TwitterClient.sharedInstance?.homeTimeline(success: { (tweets:[Tweet]) in
             self.tweets = tweets
-            for tweet in tweets {
-                print(tweet.text)
-            }
+            self.tweetsTableView.reloadData()
             }, failure: { (error: Error) in
                 
         })
@@ -30,6 +33,19 @@ class TweetsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tweets.count
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tweetCell = self.tweetsTableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCell
+        tweetCell.tweet = self.tweets[indexPath.row]
+        return tweetCell
+    }
+
     
 
     /*
